41095aef09572227fc8e7724878358381570e235
Nikolaus Albert <115626969+BiruSkai@users.noreply.github.com>
2024-02-08 09:03:29 +0700

    delete, update finished, not finished option: searchbar function

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 
 let blogList = [];
+let id = 0;
 
 app.get("/", (req, res) => {
         res.render("index.ejs")
@@ -19,27 +20,63 @@ app.get("/view_titles", (req, res) => {
 })
 
 app.get('/reading_blog/:index', (req, res) => {
-        let index = req.params.index;
+        let index = parseInt(req.params.index);
         console.log("index: " + index);
+        console.log(blogList)
+
+        const content = blogList.find(item => item.id === index)
+        console.log(content)
         
-        res.render("reading_blog.ejs", {blogList: blogList[index]});
+        res.render("reading_blog.ejs", {blogList:content});
 })
 
 app.get("/write_blog", (req, res) => {
-        res.render("write_blog.ejs")
+        res.render("write_blog.ejs", {submit:"Post Blog"})
+})
+
+app.get("/update_blog/:index", (req, res) => {
+        const index = parseInt(req.params.index);
+        const updateBlog = blogList.find(blog => blog.id === index)
+        res.render("write_blog.ejs", {blogData:updateBlog, submit:"Update Blog", index});
 })
 
 app.post("/write_blog", (req, res) => {
+        const {index, type, title, blog} = req.body;
+        console.log("type, index: ", type, index);
         
         let time = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`;
 
-        const{title, blog} = req.body;
-        let userInput = {time:time, title:title, blog:blog};
+        
+        if (type === "Post Blog") {
+                id++ ;
+                let userInput = {id, time, title, blog};
+                blogList.push(userInput)
+                console.log(blogList)
+        
+                res.render("view_titles.ejs", {blogList})
+        } else {
+                const findIndex = blogList.findIndex(blog => blog.id === parseInt(index));
+                console.log("findIndex: ", findIndex);
+                blogList[findIndex] = {
+                        id:parseInt(index),
+                        time,
+                        title,
+                        blog
+                };
+                console.log("updated blog: ", blogList[findIndex]);
+                res.redirect(`/reading_blog/${index}`);
+        }
+})
 
-        blogList.push(userInput)
-        console.log(blogList)
+app.post("/delete_blog/:index", (req, res) => {
+        let index = parseInt(req.params.index);
+        console.log("index: ", index)
 
-        res.render("view_titles.ejs", {blogList})
+        const trueIndex = blogList.findIndex(item => item.id === index);
+        console.log("trueIndex: ", trueIndex);
+        blogList.splice(trueIndex, 1);
+  
+        res.redirect("/view_titles")
 })
 
 app.listen(port, () => {
